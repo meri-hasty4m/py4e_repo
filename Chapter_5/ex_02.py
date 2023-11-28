09f9ab5de345f4df48a3f7a8a84d8d1a58f2c450
meri-hasty4m <meri@hasty4m.com>
2023-11-28 17:15:41 -0600

    chapter 5 ex

--- Chapter_5/ex_02.py
+++ Chapter_5/ex_02.py
@@ -1,20 +1,22 @@
 """
-Exercise 1: Write a program which repeatedly reads numbers until the user enters "done".
- Once "done" is entered, print out the total, count, and average of the numbers. If the user
- enters anything other than a number, detect their mistake using try and except and print
- an error message and skip to the next number.
+Exercise 2: Write another program that prompts for a list of numbers as
+ last excersise (Ch5.ex1) and at the end prints out both the maximum and minimum of the numbers
+ instead of the average.
 
 Author: Meri
 Date: Nov. 28, 2023
 Modified: N/A
 """
-
 total = 0
 count = 0
 avg = 0
+value_list = []
+list_min = None
+list_max = None
 
 while True :
     user_input = input("Enter a number: ")
+
 
     if user_input.lower() == 'done'  or user_input.lower() == 'd' :
         break
@@ -23,6 +25,7 @@ while True :
         value = int(user_input)
         count = count + 1
         total = value + total
+        value_list.append(value)
     except Exception as e:
         print("please enter a number")
         continue
@@ -30,7 +33,15 @@ while True :
     if total != 0 and count != 0 :
         avg = total / count
 
+    if value_list :
+        list_min = min(value_list)
+        list_max = max(value_list)
 
+print('-----------')
 print("Total: ", total)
 print("Count: ", count)
 print("Avg: ", avg)
+print("List Min: ", list_min)
+print("List max: ", list_max)
+print('-----------')
+print('madebymeri')
--- playground/basic_stuff/test.py
+++ playground/basic_stuff/test.py
@@ -1,18 +1,32 @@
 """
-Looping thru string and printing each letter in given word
-
+Exercise 1: Write a program which repeatedly reads numbers until the user enters "done".
+ Once "done" is entered, print out the total, count, and average of the numbers. If the user
+ enters anything other than a number, detect their mistake using try and except and print
+ an error message and skip to the next number.
 
 """
+total = 0
+count = 0
+avg = 0
+
+while True :
+    user_input = input("Enter a number: ")
+
+    if user_input.lower() == 'done'  or user_input.lower() == 'd' :
+        break
+
+    try:
+        value = int(user_input)
+        count = count + 1
+        total = value + total
+    except Exception as e:
+        print("please enter a number")
+        continue
+
+    if total != 0 and count != 0 :
+        avg = total / count
 
 
-my_string = 'Facts'
-
-def loop_string(str) :
-    i = 0
-    for letter in str :
-         letter = str[i]
-         i = i + 1
-         print(i, letter)
-
-
-loop_string(my_string)
+print("Total: ", total)
+print("Count: ", count)
+print("Avg: ", avg)
