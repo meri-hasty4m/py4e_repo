73185672fc7d4723a7dcef99100d028581e00c07
meri-hasty4m <meri@hasty4m.com>
2023-12-06 20:02:09 -0600

    working with lists

--- playground/basic_stuff/Lists/slice_list.py
+++ playground/basic_stuff/Lists/slice_list.py
@@ -1,5 +1,7 @@
 first_list = [9, 10, 39, 495, 137, 393]
+second_list = [4, 3, 4]
 first_half = first_list[:3]
 second_half = first_list[3:]
 print(first_half)
 print(second_half)
+print(first_list  + second_list)
--- playground/basic_stuff/test.py
+++ playground/basic_stuff/test.py
@@ -1,5 +1,27 @@
-friends = ["Sally", "Susie", "Samantha"]
+# Write a program that takes in user test scores prints average of scores.
 
-for friend in friends :
-    print(friend)
-    
+'''
+Author: Meri
+Date: Dec. 6, 2023
+Modified: N/A
+
+
+'''
+
+scores = list()
+
+math = input('Math score: ')
+scores.append(int(math))
+english = input('English score: ')
+scores.append(int(english))
+science = input('Science score: ')
+scores.append(int(science))
+
+print(scores)
+
+# replace the values with new values. Mupltiplying the scores by 2
+# and printing out the list with the updated values 
+
+for score in range(len(scores)) :
+    scores[score] = scores[score] * 2
+print(scores)
